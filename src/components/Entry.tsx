248bdb77b7fb40f1262c86c1ff7c428b6e0f8dd2
Enes Kutay Sezen <eneskutaysezen@gmail.com>
2022-07-14 19:21:36 -0400

    Display coffee name

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -15,7 +15,7 @@ const Entry = (props: { entry: any }) => {
   return (
     <div className="flex justify-between items-center mx-4 my-2 px-3 py-2 border border-base-300 rounded bg-base-200">
       <div>
-        <p className="capitalize">{entryParsed.brewMethod}</p>
+        <p className="capitalize">{entryParsed.coffeeName} - {entryParsed.brewMethod}</p>
         <div className="flex flex-row space-x-2 sm:space-x-0 sm:flex-col">
           <p>In: {entryParsed.coffeeIn}</p>
           <p>Out: {entryParsed.coffeeOut}</p>
