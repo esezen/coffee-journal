10cfb5adfdff4a2b5b1d41745d6832b5316d1e32
Enes Kutay Sezen <eneskutaysezen@gmail.com>
2022-07-13 23:14:45 -0400

    Styling

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,10 +1,17 @@
-import { CheckCircleIcon } from '@heroicons/react/outline';
+import { CheckCircleIcon, XCircleIcon, MinusCircleIcon } from '@heroicons/react/outline';
 
-const Entry = (props) => {
+const Entry = (props: any) => {
   const { entry } = props;
 
+  const renderResultIcon = (result: string) => {
+    if (result === 'Good') return <CheckCircleIcon className="h-8 w-8 text-green-600" />;
+    if (result === 'Meh') return <MinusCircleIcon className="h-8 w-8 text-gray-600" />;
+    if (result === 'Bad') return <XCircleIcon className="h-8 w-8 text-red-600" />;
+    return '';
+  };
+
   return (
-    <div className="flex justify-between items-center mx-4 my-2 px-3 py-2 border rounded bg-gray-200">
+    <div className="flex justify-between items-center mx-4 my-2 px-3 py-2 border border-base-300 rounded bg-base-200">
       <div>
         <p className="capitalize">{entry.type}</p>
         <div className="flex flex-row space-x-2 sm:space-x-0 sm:flex-col">
@@ -13,7 +20,7 @@ const Entry = (props) => {
           <p>Time: {entry.brewTime}</p>
         </div>
       </div>
-      <CheckCircleIcon className="h-6 w-6 text-green-600" />
+      { renderResultIcon(entry.result) }
     </div>
   );
 };
