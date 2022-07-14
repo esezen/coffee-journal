ea12fb5bc1ccc7d1bce105401ef77033d84c758b
Enes Kutay Sezen <eneskutaysezen@gmail.com>
2022-07-13 23:32:54 -0400

    Set meta tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import Head from 'next/head';
 
 interface Props {
   children: React.ReactNode,
@@ -10,6 +11,9 @@ const Layout = (props: Props) => {
 
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      </Head>
       <div className="w-full text-2xl font-bold h-12 text-center flex justify-center space-x-6 items-center border-b mb-4">
         <Link href="/">
           Coffee Journal
